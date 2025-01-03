982773d2f3c8b74b7ac679a8f93954ac332a4216
Jash Bhavsar <jsonaniintwala@gmail.com>
2025-01-02 20:37:29 -0500

    Initial Push
    -Initial Push
    - Simple Calculator with basic functions like: Addition, Subtraction, Multiplication, and Division
    - Added JavaScript, html and CSS files

---
+++ script.js
@@ -0,0 +1,21 @@
+const resultField = document.getElementById('result');
+
+function append(value) {
+  resultField.value += value;
+}
+
+function clearResult() {
+  resultField.value = '';
+}
+
+function deleteLast() {
+  resultField.value = resultField.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    resultField.value = eval(resultField.value);
+  } catch {
+    resultField.value = 'Error';
+  }
+}
